5d5114ffdc7e3e49b1d703d80e87a445da61b984
Ranjith <mailranjith7022@gmail.com>
2024-11-17 15:58:09 +0530

    inital game setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+def main():
+    print("Starting asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
